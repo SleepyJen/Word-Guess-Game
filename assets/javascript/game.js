c1d476a28f9fbb8f339f719117d755c32a3a0f85
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-02 22:41:47 -0700

    created a holder to hide letters and onkeyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,17 @@
 //word bank
 var disney = ["The Little Mermaid", "Sleeping Beauty", "Snow White", "Cinderella",
-    "Alice in Wonderland", "Frozen", "The Lion King", "Aladdin", "Pirates of the Carribean"];
+    "Alice in Wonderland", "Frozen", "The Lion King", "Aladdin", "Pirates of the Carribean", "Moana"];
 var games = ["Borderlands", "Kingdom Hearts", "Final Fantasy", "Call of Duty",
-    "Beatsaber", "Smash", "CS go", "League of Legends", "TFT"];
-var pixar = ["Cars", "Toy Story", "Coco", "Mosters Inc", "Incredibles",
-    "Inside Out", "Finding Nemo", "Brave", "Up", "Ratatouille"];
+    "Beatsaber", "Smash Bros", "CS go", "League of Legends", "TFT", "World of Warcraft"];
+var pixar = ["Cars", "Toy Story", "Coco", "Monsters Inc", "Incredibles",
+    "Inside-Out", "Finding Nemo", "Brave", "Up", "Ratatoulli"];
+var alphabet = ["a", "b", "c", "d"];
 
 var box = document.querySelector('#body');
 var choice = document.querySelector('#start');
+var guess = document.querySelector('#wrong');
+var show = document.querySelector('#cate');
+var key;
 
 function chooseRandom() {
     var rand = Math.floor(Math.random() * 10);
@@ -19,8 +23,10 @@ function chooseRandom() {
     return pixar;
 }
 
-box.onkeyup = function () {
+function game() {
     var category = chooseRandom();
+    var element, elementHolder = [];
+    var done, key;
     if (category === disney) {
         choice.textContent = "I'm thinking of a Disney Movie";
     } else if (category === games) {
@@ -28,4 +34,29 @@ box.onkeyup = function () {
     } else {
         choice.textContent = "I'm thinking of a Pixar Movie";
     }
-};
+
+    element = category[Math.floor(Math.random() * 10)];
+
+    for (var i = 0; i < element.length; i++) {
+        if (element[i] === " ") {
+            elementHolder.push(element[i]);
+        } else {
+            elementHolder.push("_");
+        }
+    }
+
+    box.onkeyup = function (event) {
+        key = event.key;
+        for (var j = 0; j < element.length; j++) {
+            if (key === element[j]) {
+                elementHolder[j] = element[j];
+            }
+        }
+        show.textContent = elementHolder.join(" ");
+    };
+
+    console.log(key);
+
+}
+
+game();
